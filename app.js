6b86093de0d5eab4d1734daa5113852bdd675d76
danmedie <94713071+danmedie@users.noreply.github.com>
2021-12-17 09:14:19 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+document.querySelector("#hamburger-button").addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("opacity-0");
+});
